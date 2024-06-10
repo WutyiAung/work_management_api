35af1bc0ec799cf10add3011ce10e4b58c058bd4
khcoding <programmercruso15@gmail.com>
2024-06-10 11:12:05 +0630

    report modify with status and is_reported column added at assignedTask table

--- app/Http/Controllers/ReportApiController.php
+++ app/Http/Controllers/ReportApiController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Report;
-use App\Models\Shooting;
+use Carbon\Carbon;
 use App\Models\AssignedTask;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
@@ -11,14 +11,23 @@ use App\Http\Requests\ReportRequest;
 use Illuminate\Support\Facades\File;
 use App\Models\ShootingAccessoryCategory;
 
-use Carbon\Carbon;
+
 class ReportApiController extends Controller
 {
     //Create
-    public function reportCreate(ReportRequest $request){
+    public function reportCreate(ReportRequest $request)
+    {
         $assignedTask = AssignedTask::where('id',$request->assigned_task_id)->with('shooting')->first();
-        $assignedTask->status = $request->status;
+        // $assignedTask->status = $request->status;
         $assignedTask->progress = $request->progress;
+        $assignedTask->is_reported = '1';
+        // Check if progress is 100, and if so, set status to 'done'
+        if ($request->progress == '100') {
+            $request->status = 'done';  // Set request status to 'done'
+            $assignedTask->status = 'done';  // Set assigned task status to 'done'
+        } else {
+            $assignedTask->status = $request->status;
+        }
         $assignedTask->save();
         $validatedData = $request->validated();
         if($request->hasFile('attachment_path')){
@@ -82,6 +91,83 @@ class ReportApiController extends Controller
         $shooting->shootingAccessoryCategories()->sync($categoryIds);
         Log::info($request);
     }
+    // public function index(Request $request)
+    // {
+    //     $taskId = $request->query('task_id');
+    //     $id = $request->query('id');
+    //     $employeeId = $request->query('employee_id');
+    //     $fromDate = $request->query('from_date');
+    //     $toDate = $request->query('to_date');
+
+    //     if ($id && $id !== 'undefined' && $id !== 'null') {
+    //         $report = Report::with('task.shooting.shootingAccessoryCategories')->find($id);
+    //         if (!$report) {
+    //             return response()->json([
+    //                 "status" => 404,
+    //                 "message" => "Report not found"
+    //             ], 404);
+    //         }
+    //         // If shootingData is not empty, get the first item
+    //         $report->task->shootingData = $report->task->shooting->isNotEmpty() ? $report->task->shooting->first() : null;
+    //         unset($report->task->shooting);
+
+    //         // Rename shootingAccessoryCategories to shooting_accessories
+    //         if ($report->task->shootingData) {
+    //             $report->task->shootingData->shooting_accessories = $report->task->shootingData->shootingAccessoryCategories;
+    //             unset($report->task->shootingData->shootingAccessoryCategories);
+    //         }
+    //         // Convert is_reported field to boolean
+    //         $report->task->is_reported = $report->task->is_reported === "1";
+
+    //         return response()->json([
+    //             "status" => "success",
+    //             "report" => $report
+    //         ]);
+    //     }
+
+    //     $query = Report::with('task.shooting.shootingAccessoryCategories');
+    //     if ($taskId && $taskId !== 'undefined' && $taskId !== 'null') {
+    //         $query->where('assigned_task_id', $taskId);
+    //     }
+    //     if ($employeeId && $employeeId !== 'undefined' && $employeeId !== 'null') {
+    //         $query->where('user_id', $employeeId);
+    //     }
+    //     if ($fromDate && $fromDate !== 'undefined' && $fromDate !== 'null') {
+    //         $fromDate = Carbon::parse($fromDate)->startOfDay();
+    //         if ($toDate && $toDate !== 'undefined' && $toDate !== 'null') {
+    //             $toDate = Carbon::parse($toDate)->endOfDay();
+    //             $query->whereBetween('created_at', [$fromDate, $toDate]);
+    //         } else {
+    //             $query->whereDate('created_at', $fromDate);
+    //         }
+    //     }
+    //     $reports = $query->get();
+    //     if ($reports->isEmpty()) {
+    //         return response()->json([
+    //             "status" => 404,
+    //             "message" => "No reports found"
+    //         ], 404);
+    //     }
+    //     $reports->each(function ($report) {
+    //         // If shootingData is not empty, get the first item
+    //         $report->task->shootingData = $report->task->shooting->isNotEmpty() ? $report->task->shooting->first() : null;
+    //         unset($report->task->shooting);
+
+    //         // Rename shootingAccessoryCategories to shooting_accessories
+    //         if ($report->task->shootingData) {
+    //             $report->task->shootingData->shooting_accessories = $report->task->shootingData->shootingAccessoryCategories;
+    //             unset($report->task->shootingData->shootingAccessoryCategories);
+    //         }
+    //     });
+    //     return response()->json([
+    //         "status" => "success",
+    //         "reports" => $reports
+    //     ]);
+    // }
+    // use Carbon\Carbon;
+    // use Illuminate\Http\Request;
+    // use App\Models\Report;
+
     public function index(Request $request)
     {
         $taskId = $request->query('task_id');
@@ -98,16 +184,7 @@ class ReportApiController extends Controller
                     "message" => "Report not found"
                 ], 404);
             }
-            // If shootingData is not empty, get the first item
-            $report->task->shootingData = $report->task->shooting->isNotEmpty() ? $report->task->shooting->first() : null;
-            unset($report->task->shooting);
-
-            // Rename shootingAccessoryCategories to shooting_accessories
-            if ($report->task->shootingData) {
-                $report->task->shootingData->shooting_accessories = $report->task->shootingData->shootingAccessoryCategories;
-                unset($report->task->shootingData->shootingAccessoryCategories);
-            }
-
+            $this->processReport($report);
             return response()->json([
                 "status" => "success",
                 "report" => $report
@@ -138,15 +215,7 @@ class ReportApiController extends Controller
             ], 404);
         }
         $reports->each(function ($report) {
-            // If shootingData is not empty, get the first item
-            $report->task->shootingData = $report->task->shooting->isNotEmpty() ? $report->task->shooting->first() : null;
-            unset($report->task->shooting);
-
-            // Rename shootingAccessoryCategories to shooting_accessories
-            if ($report->task->shootingData) {
-                $report->task->shootingData->shooting_accessories = $report->task->shootingData->shootingAccessoryCategories;
-                unset($report->task->shootingData->shootingAccessoryCategories);
-            }
+            $this->processReport($report);
         });
         return response()->json([
             "status" => "success",
@@ -154,7 +223,49 @@ class ReportApiController extends Controller
         ]);
     }
 
-    public function reportUpdate(ReportRequest $request, $id){
+    private function filterQuery($queryParam)
+    {
+        return ($queryParam && $queryParam !== 'undefined' && $queryParam !== 'null') ? $queryParam : null;
+    }
+
+    private function processReport(&$report)
+    {
+        // If shootingData is not empty, get the first item
+        $report->task->shootingData = $report->task->shooting->isNotEmpty() ? $report->task->shooting->first() : null;
+        unset($report->task->shooting);
+
+        // Rename shootingAccessoryCategories to shooting_accessories
+        if ($report->task->shootingData) {
+            $report->task->shootingData->shooting_accessories = $report->task->shootingData->shootingAccessoryCategories;
+            unset($report->task->shootingData->shootingAccessoryCategories);
+        }
+
+        // Convert is_reported field to boolean
+        $report->task->is_reported = $report->task->is_reported === "1";
+
+        // Generate URLs for file paths
+        $report->imageUrl = url('file/' . $report->photo_path);
+        $report->videoUrl = url('file/' . $report->video_path);
+        $report->documentUrl = url('file/' . $report->attachment_path);
+
+        // If the report has a project, generate the file URL
+        if (isset($report->project)) {
+            $report->project->fileURL = url('file/' . $report->project->document);
+        }
+
+        // If the report has a shootingData, generate the file URL
+        if (isset($report->task->shootingData)) {
+            $report->task->shootingData->fileURL = url('file/' . $report->task->shootingData->document);
+        }
+
+        // If the report has a user, generate the image URL
+        if (isset($report->user)) {
+            $report->user->imgURL = url('file/' . $report->user->photo_path);
+        }
+    }
+
+    public function reportUpdate(ReportRequest $request, $id)
+    {
         $report = Report::findOrFail($id);
         $validatedData = $request->validated();
         if($request->hasFile('attachment_path')){
@@ -204,7 +315,8 @@ class ReportApiController extends Controller
             "report" => $report
         ]);
     }
-    public function reportDelete($id){
+    public function reportDelete($id)
+    {
         Report::findOrFail($id)->delete();
         $reports = Report::get();
         return response()->json([
